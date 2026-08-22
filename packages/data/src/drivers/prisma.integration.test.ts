dd101e5713d6f4a236a66ab78f3a0e94689b10cb
Bbenrod <benjamintec3455@gmail.com>
2026-08-22 10:10:52 -0600

    test: clean persisted activity in integration teardown

--- packages/data/src/drivers/prisma.integration.test.ts
+++ packages/data/src/drivers/prisma.integration.test.ts
@@ -134,6 +134,7 @@ async function seedReferenceData(prisma: PrismaClient) {
 }
 
 async function cleanDatabase(prisma: PrismaClient) {
+  await prisma.tlozActivityEvent.deleteMany();
   await prisma.content.deleteMany();
   await prisma.container.deleteMany();
   await prisma.tlozUserMissionState.deleteMany();
